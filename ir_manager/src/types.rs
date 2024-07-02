1796c6e9651d68d49ed2b8b02f4f7aa0ec9d1d38
EmperorOrokuSaki <artie.eth@gmail.com>
2024-07-02 17:04:46 +0400

    feat: add new liquity functions

--- ir_manager/src/types.rs
+++ ir_manager/src/types.rs
@@ -1,6 +1,6 @@
 use alloy_primitives::U256;
 use alloy_sol_types::sol;
-use candid::CandidType;
+use candid::{CandidType, Principal};
 
 use crate::evm_rpc::RpcError;
 
@@ -24,17 +24,27 @@ pub struct StrategyData {
     pub upfront_fee_period: U256,
     pub eoa_nonce: u64,
     pub eoa_pk: Option<String>,
-    pub last_update: u64
+    pub last_update: u64,
 }
 
 #[derive(CandidType)]
 pub struct StrategyInput {
     pub upfront_fee_period: String, // Because IC does not accept U256 params.
-    pub target_min: String, // Because IC does not accept U256 params.
+    pub target_min: String,         // Because IC does not accept U256 params.
+}
+
+#[derive(CandidType)]
+pub struct InitArgs {
+    pub rpc_principal: Principal,
+    pub rpc_url: String,
+    pub managers: Vec<String>,
+    pub multi_trove_getters: Vec<String>,
+    pub collateral_registry: String,
+    pub strategies: Vec<StrategyInput>,
 }
 
 sol!(
-    // Liquity
+    // Liquity types
     struct CombinedTroveData {
         uint256 id;
         uint256 debt;
@@ -43,12 +53,22 @@ sol!(
         uint256 snapshotETH;
         uint256 snapshotBoldDebt;
     }
+
+    // Liquity getters
     function getRedemptionRateWithDecay() public view override returns (uint256);
     function getEntireSystemDebt() public view returns (uint256 entireSystemDebt);
     function getUnbackedPortionPriceAndRedeemability() external returns (uint256, uint256, bool);
     function getMultipleSortedTroves(int256 _startIdx, uint256 _count) external view returns (CombinedTroveData[] memory _troves);
     function getTroveAnnualInterestRate(uint256 _troveId) external view returns (uint256);
-
+    
+    // Liquity externals
+    function setBatchManagerAnnualInterestRate(
+        uint128 _newAnnualInterestRate,
+        uint256 _upperHint,
+        uint256 _lowerHint,
+        uint256 _maxUpfrontFee
+    ) external;
+    
     // ckETH Helper
     function deposit(bytes32 _principal) public payable;
 );
