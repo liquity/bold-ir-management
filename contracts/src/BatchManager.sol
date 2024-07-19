e8fbe506fb92d1ff5bb290d4b3b90ddf515cfd87
EmperorOrokuSaki <artie.eth@gmail.com>
2024-07-19 16:20:26 +0400

    feat: constructor and claimBold functions for the batch manager.

--- contracts/src/BatchManager.sol
+++ contracts/src/BatchManager.sol
@@ -3,21 +3,30 @@
 pragma solidity 0.8.20;
 
 import "./Interfaces/IBoldToken.sol";
+import "./Interfaces/ITroveManager.sol";
 
 /**
  * @title Liquity V2 Autonomous Interest Rate Manager
  * @dev Allows for BOLD<>ETH conversions with a discounted rate and the distribution of the collected ether to the corresponding EOA.
  */
 contract BatchManager {
-    address immutable batchManager;
-    address immutable boldERC20;
-    address immutable troveManager;
+    address immutable batchManagerEOA;
+
+    IBorrowerOperations immutable borrowerOperations;
+    ITroveManager immutable troveManager;
+    IBoldToken immutable boldToken;
+
     // event for EVM logging
-    event initialized(address batchManager, address boldERC20, address troveManager);
+    event initialized(
+        address batchManagerEOA,
+        address boldToken,
+        address troveManager,
+        address borrowerOperations
+    );
 
     // modifier to check if caller is the batch manaager
-    modifier isBatchManager() {
-        require(msg.sender == batchManager, "Caller is not batch manager.");
+    modifier isBatchManagerEOA() {
+        require(msg.sender == batchManagerEOA, "Caller is not batch manager.");
         _;
     }
 
@@ -25,14 +34,22 @@ contract BatchManager {
      * @dev Set contract deployer as owner
      */
     constructor(
-        address batchManagerArg,
-        address boldERC20Arg,
-        address troveManagerArg
+        address _batchManagerEOA,
+        ITroveManager _troveManager,
+        IBorrowerOperations _borrowerOperations,
+        IBoldToken _boldToken
     ) {
-        batchManager = batchManagerArg;
-        boldERC20 = boldERC20Arg;
-        troveManager = troveManagerArg;
-        emit initialized(batchManager, boldERC20, troveManager);
+        batchManagerEOA = _batchManagerEOA;
+        troveManager = _troveManager;
+        borrowerOperations = _borrowerOperations;
+        boldToken = _boldToken;
+
+        emit initialized(
+            batchManagerEOA,
+            address(boldToken),
+            address(troveManager),
+            address(borrowerOperations)
+        );
     }
 
     /**
@@ -40,6 +57,26 @@ contract BatchManager {
      */
     function claimBOLD() external payable {
         uint256 sentEther = msg.value;
-        
+        // check current weth/bold rate
+
+        // check current bold holdings
+        uint256 boldHoldings = boldToken.balanceOf(address(this));
+        uint256 expectedBold = 1; // TODO: Query the WETH rate from the oracle contract and calculate the expected bold
+        if (boldHoldings >= expectedBold) {
+            // we have enough bold
+            boldToken.transfer(msg.sender, expectedBold);
+            return;
+        }
+        uint256 accruedBold = troveManager
+            .getLatestBatchData(address(this))
+            .accruedManagementFee;
+
+        if (accruedBold + boldHoldings >= expectedBold) {
+            borrowerOperations.applyBatchInterestAndFeePermissionless(
+                address(this)
+            );
+            boldToken.transfer(msg.sender, expectedBold);
+            return;
+        }
     }
 }
