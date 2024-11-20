28246fba62a70829929490c24c3ecdc5011cc352
EmperorOrokuSaki <me@nimara.xyz>
2024-11-20 21:35:09 +0400

    feat: transaction builder

---
+++ ir_manager/src/utils/transaction_builder.rs
@@ -0,0 +1,123 @@
+//! Transaction builder (and sender) that interacts with the EVM RPC canister
+
+use std::str::FromStr;
+
+use alloy::consensus::TxEip1559;
+use alloy_primitives::{Address, Bytes, TxKind, U256};
+use evm_rpc_types::{MultiRpcResult, RpcServices};
+use ic_exports::ic_cdk::api::management_canister::ecdsa::{EcdsaCurve, EcdsaKeyId};
+
+use crate::{constants::CHAIN_ID, types::DerivationPath};
+
+use super::{
+    common::get_rpc_services,
+    error::{ManagerError, ManagerResult},
+    evm_rpc::{SendRawTransactionStatus, Service},
+    gas::{estimate_transaction_fees, get_estimate_gas, FeeEstimates},
+    signer::sign_eip1559_transaction,
+};
+
+/// Transaction builder struct
+#[derive(Default)]
+pub struct TransactionBuilder {
+    to: String,
+    from: String,
+    data: Vec<u8>,
+    value: U256,
+    nonce: u64,
+    derivation_path: DerivationPath,
+    cycles: u128,
+}
+
+impl TransactionBuilder {
+    /// Sets the `to` field
+    pub fn to(mut self, to: String) -> Self {
+        self.to = to;
+        self
+    }
+
+    /// Sets the `from` field
+    pub fn from(mut self, from: String) -> Self {
+        self.from = from;
+        self
+    }
+
+    /// Sets the `data` field
+    pub fn data(mut self, data: Vec<u8>) -> Self {
+        self.data = data;
+        self
+    }
+
+    /// Sets the `value` field
+    pub fn value(mut self, value: U256) -> Self {
+        self.value = value;
+        self
+    }
+
+    /// Sets the `nonce` field
+    pub fn nonce(mut self, nonce: u64) -> Self {
+        self.nonce = nonce;
+        self
+    }
+
+    /// Sets the `derivation_path` field
+    pub fn derivation_path(mut self, derivation_path: DerivationPath) -> Self {
+        self.derivation_path = derivation_path;
+        self
+    }
+
+    /// Sets the `cycles` field
+    pub fn cycles(mut self, cycles: u128) -> Self {
+        self.cycles = cycles;
+        self
+    }
+
+    /// Builds the TransactionBuilder into a Transaction and sends it
+    pub async fn send(
+        self,
+        rpc_canister: &Service,
+    ) -> ManagerResult<MultiRpcResult<SendRawTransactionStatus>> {
+        let chain_id = CHAIN_ID;
+        let input = Bytes::from(self.data.clone());
+        let rpc: RpcServices = get_rpc_services();
+
+        let FeeEstimates {
+            max_fee_per_gas,
+            max_priority_fee_per_gas,
+        } = estimate_transaction_fees(9, rpc.clone(), rpc_canister).await?;
+
+        let estimated_gas =
+            get_estimate_gas(rpc_canister, self.data, self.to.clone(), self.from).await?;
+
+        let key_id = EcdsaKeyId {
+            curve: EcdsaCurve::Secp256k1,
+            name: String::from("key_1"),
+        };
+
+        let request = TxEip1559 {
+            chain_id,
+            to: TxKind::Call(
+                Address::from_str(&self.to)
+                    .map_err(|err| ManagerError::DecodingError(format!("{:#?}", err)))?,
+            ),
+            max_fee_per_gas,
+            max_priority_fee_per_gas,
+            value: self.value,
+            nonce: self.nonce,
+            gas_limit: estimated_gas.to::<u128>(),
+            access_list: Default::default(),
+            input,
+        };
+
+        let signed_transaction =
+            sign_eip1559_transaction(request, key_id, self.derivation_path).await;
+
+        match rpc_canister
+            .eth_send_raw_transaction(rpc, None, signed_transaction, self.cycles)
+            .await
+        {
+            Ok((response,)) => Ok(response),
+            Err(e) => Err(ManagerError::Custom(e.1)),
+        }
+    }
+}
